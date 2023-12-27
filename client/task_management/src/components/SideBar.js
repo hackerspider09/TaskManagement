c4810e1ea10e8c12443cb27b4bae24848777ac29
prasad <hackerspider09@gmail.com>
2023-12-27 17:29:40 +0530

    phone btn added

--- client/task_management/src/components/SideBar.js
+++ client/task_management/src/components/SideBar.js
@@ -9,11 +9,14 @@ import AddIcon from '@mui/icons-material/Add';
 
 import LiComponent from "./LiComponent"
 
+import CloseIcon from '@mui/icons-material/Close';
+
 
 const SideBar = () => {
     const [channelList,setChannelList] = useState(null)
     const [channelBtn,setChannelBtn] = useState(false)
     const {setChannelAdminMap} = useContext(APIContext)
+    const [sideBarBtnClick,setSideBarBtnClick] = useState(true)
 
     const navigate = useNavigate()
     const signOut =()=>{
@@ -63,16 +66,21 @@ const SideBar = () => {
   return (
     <div className='pt-20'>
 
-    <button data-drawer-target="sidebar-multi-level-sidebar" data-drawer-toggle="sidebar-multi-level-sidebar" aria-controls="sidebar-multi-level-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+    <button data-drawer-target="sidebar-multi-level-sidebar" data-drawer-toggle="sidebar-multi-level-sidebar" aria-controls="sidebar-multi-level-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm rounded-lg sm:hidden hover:bg-bgColor5 focus:outline-none focus:ring-2 focus:ring-gray-200 text-bgColor4  focus:ring-gray-600" onClick={()=>{setSideBarBtnClick(!sideBarBtnClick)}}>
     <span className="sr-only">Open sidebar</span>
     <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
     <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
     </svg>
     </button>
 
-    <aside id="sidebar-multi-level-sidebar" className=" fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+    <aside id="sidebar-multi-level-sidebar" className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${sideBarBtnClick ? '-translate-x-full' : null} sm:translate-x-0`} aria-label="Sidebar">
     <div className="h-full px-3 py-4 overflow-y-auto bg-bgColor5   pt-20">
         <ul className="space-y-2 font-medium">
+            {!sideBarBtnClick ? 
+            <li className='flex justify-end text-white'>
+                <span onClick={()=>{setSideBarBtnClick(!sideBarBtnClick)}}><CloseIcon /></span>
+            </li>
+            : null}
             <li>
                 <a href="/channel/create/" className="flex items-center p-2  rounded-lg text-white  dark:hover:bg-bgColor6 group">
                 <svg className="w-5 h-5 transition duration-75 text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
